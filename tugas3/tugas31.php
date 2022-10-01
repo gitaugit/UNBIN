8a2b3496753bb058a8457123ae60e23308e9a266
gitaugit <113131530+gitaugit@users.noreply.github.com>
2022-10-01 16:57:09 +0700

    Create tugas31.php

---
+++ tugas3/tugas31.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table>    
+<tr>
+<td><?php
+for($i=1;$i<=5; $i++){
+ for($u=1; $u<=$i; $u++){
+echo "*";
+}
+echo "<br />";
+
+}
+?></td>
+
+
+
+<td><?php
+for($i=1;$i<=4; $i++){
+ for($u=1; $u<=$i; $u++){
+echo "+";
+}
+echo "<br />";
+
+}
+?></td>
+</tr>
+</table>
+</body>
+</html>
